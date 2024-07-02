ee97b274f4a9a9166d31f9d27ffd1400d64983dc
Adrien Robert <a.robert@rt-iut.re>
2024-07-02 14:25:19 +0200

    debug

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Log::info('CSRF Token (csrf_token()): ' . csrf_token());
+Log::info('CSRF Token (session token): ' . $request->session()->token());
+Log::info('Session ID: ' . $request->session()->getId());
+
+
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+
+    Log::info('Authenticated User: ' . $request->user());
     return $request->user();
 });
